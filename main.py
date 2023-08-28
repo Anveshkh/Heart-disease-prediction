0fe7750c24c78806ca02af0ccee4e3c9b7d8c691
Anvesh Khambadkar <khambadkaranvesh@gmail.com>
2023-08-28 16:54:47 +0530

    Feature standardized

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, precision_score
+from sklearn.preprocessing import StandardScaler
 np.random.seed(42)
 
 pd.set_option("display.max_columns", None)
@@ -152,3 +153,15 @@ print("Naive Bayes Accuracy score = ", round(gnb_acc_score, 2))
 print("Naive Bayes Precision score = ", round(gnb_precision_sc, 2))
 print("Naive Bayes Classification report = \n", gnb_class_report)
 
+## Scaling the data in the range of 0-1
+
+scaler = StandardScaler()
+scaled_df = scaler.fit_transform(heart_disease.drop("target", axis=1))
+cols = X_train.columns
+new_df = pd.DataFrame(scaled_df, columns=cols)
+
+new_X_train, new_X_test, new_y_train, new_y_test = train_test_split(new_df, Y, test_size=0.2)
+
+model.fit(new_X_train, new_y_train)
+
+print("Performance after Standardization = ", model.score(new_X_test, new_y_test))
